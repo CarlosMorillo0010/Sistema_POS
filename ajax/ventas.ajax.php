c8506c16a30ecf41ac39fbf59ba3db964c343c4b
FabianIIX <83839614+FabianIIX@users.noreply.github.com>
2025-09-05 12:38:38 -0400

    Libro de venta, modal de detalles

--- ajax/ventas.ajax.php
+++ ajax/ventas.ajax.php
@@ -1,13 +1,18 @@
 <?php
 
-require_once "../controller/ventas.controller.php";
-require_once "../model/ventas.model.php";
+// Requerimientos corregidos y aumentados para asegurar que todas las dependencias existan.
+require_once "../model/connection.php";
+require_once "../controller/ventas-controller.php";
+require_once "../model/ventas-model.php";
+require_once "../controller/clients.controller.php";
+require_once "../model/clients.model.php";
+
 
 class AjaxVentas{
 
     /*======================================
-     TRAER VENTA
-    ======================================**/
+     TRAER VENTA (Usado en otras partes del sistema)
+    ======================================*/
     public $idVenta;
 
     public function ajaxTraerVenta(){
@@ -23,12 +28,28 @@ class AjaxVentas{
 }
 
 /*======================================
- OBJETO
-======================================**/
+ OBJETO PARA TRAER VENTA SIMPLE
+======================================*/
 if(isset($_POST["idVenta"])){
 
     $traerVenta = new AjaxVentas();
     $traerVenta -> idVenta = $_POST["idVenta"];
     $traerVenta -> ajaxTraerVenta();
+
+}
+
+/*======================================
+ TRAER VENTA DETALLADA PARA MODAL
+======================================*/
+if(isset($_POST["idVentaDetalle"])){
+
+    $item = "id_venta";
+    $valor = $_POST["idVentaDetalle"];
+
+    // Esta es la función correcta que trae todos los datos necesarios para el modal.
+    $ventaDetallada = ControllerVentas::ctrMostrarVentaDetallada($item, $valor);
+
+    // Devolvemos el resultado como un JSON válido.
+    echo json_encode($ventaDetallada);
 
 }
